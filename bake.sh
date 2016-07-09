2810156065721283346606edfdd94979995c0008
Rumkin <hello@rumk.in>
2016-07-09 16:19:46 +0000

    src: update bakefile to v 0.11.0

--- bake.sh
+++ bake.sh
@@ -1,29 +1,29 @@
-function __init {
+task:init() {
     __initial_deps
     npm init
 }
 
-function __initial_deps {
+task:initial_deps() {
     set -e
     bake dev mocha
     bake dev istanbul
 }
 
 # Install node package
-function __i {
+task:i() {
     npm i $@
 }
 
 # Install dev dependency
-function __dev {
+task:dev() {
     npm i --save-dev $@
 }
 
-function __cov {
+task:cov() {
     npm run cov
 }
 
-function __sync {
+task:sync() {
     TARGET=${1:=stage}
     npm sync -- $1
     ssh -i /projects/.ssh/hyphen hyphen@http://78.47.172.45/ 'pm2 restart ~/githook'
